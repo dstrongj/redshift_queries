12ca54d976c89582e22620e5057cad35f48f71d3
Daniel <daniel.strong4@gmail.com>
2018-12-09 07:18:50 -0800

    Add files via upload

---
+++ user_management.sql
@@ -0,0 +1,22 @@
+
+
+
+##list current users
+
+select * from pg_user;
+
+
+##create new users
+
+CREATE USER username with password 'password'  in group dbgroupnamehere;
+commit
+
+##change user password
+
+ALTER USER username password 'password';
+commit
+
+##remove from database
+
+DROP USER username;
+commit
